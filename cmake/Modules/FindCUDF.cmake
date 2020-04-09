f481be39adcd4e582197289fe24672044a381cc0
Jeremy Dyer <jdye64@gmail.com>
2020-04-09 18:02:10 -0400

    updates

--- cmake/Modules/FindCUDF.cmake
+++ cmake/Modules/FindCUDF.cmake
@@ -11,13 +11,22 @@ find_path(CUDF_INCLUDE_DIR cudf/cudf.h
 find_library(CUDF_LIBRARY NAMES cudf libcudf
              HINTS ${CONDA_LINK_DIRS} /usr/local/lib )
 
+# RMM
+find_path(RMM_INCLUDE_DIR rmm/rmm.h
+          HINTS ${CONDA_INCLUDE_DIRS} /usr/local/include
+          PATH_SUFFIXES rmm)
+
+find_library(RMM_LIBRARY NAMES rmm librmm
+          HINTS ${CONDA_LINK_DIRS} /usr/local/lib )
+
 include(FindPackageHandleStandardArgs)
+
 # handle the QUIETLY and REQUIRED arguments and set LIBCUDF_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(CUDF DEFAULT_MSG
-                                  CUDF_LIBRARY CUDF_INCLUDE_DIR)
+                                  CUDF_LIBRARY CUDF_INCLUDE_DIR RMM_INCLUDE_DIR RMM_LIBRARY)
 
-mark_as_advanced(CUDF_INCLUDE_DIR CUDF_LIBRARY )
+mark_as_advanced(CUDF_INCLUDE_DIR CUDF_LIBRARY RMM_INCLUDE_DIR RMM_LIBRARY)
 
-set(CUDF_LIBRARIES ${CUDF_LIBRARY} )
-set(CUDF_INCLUDE_DIRS ${CUDF_INCLUDE_DIR} )
+set(CUDF_LIBRARIES ${CUDF_LIBRARY} ${RMM_LIBRARY})
+set(CUDF_INCLUDE_DIRS ${CUDF_INCLUDE_DIR} ${RMM_INCLUDE_DIR})
--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 // A very basic and start forward C++ example for using RapidsAI libcudf
 
 #include <iostream>
-#include <cudf/cudf.h>
+#include <cudf/table/table.hpp>
+#include <cudf/io/types.hpp>
+#include <cudf/io/functions.hpp>
 
 int main() {
     printf("RapidsAI Simple libcudf Example\n");
 
-    // Read in sample csv file to libcudf dataframe
+    // Make an alias for the io namespace to make more readable
+    namespace io = cudf::experimental::io;
 
-    // Perform filter and group by aggregation
+    // 1 - Read in sample csv file to libcudf dataframe
 
-    // Write results back to csv file
+    // 1.1 First we define something called "csv_read_arg" this tells 
+    // the io_function reader all the parameters its needs to know to 
+    // understand how to read and parse the incoming csv data
+    // Reference: https://github.com/rapidsai/cudf/blob/cc3855d01d04c7c93f526e5046a75e60a8040501/cpp/include/cudf/io/functions.hpp#L133
+    std::string filepath = "/home/jdyer/Development/libcudf-examples/data/weather/2000.csv.gz";
+    io::read_csv_args in_args{io::source_info{filepath}};
+
+    // 1.2 Set all of the values for the csv reader arguments
+    //in_args.header = -1;
+
+    // 1.3 Perform the actual read which create the cudf::table instance for you. Same thing as Python "DataFrame" instance
+    //io::table_with_metadata gdf = io::read_csv(args);
+    auto result = io::read_csv(in_args);
+
+    // 2 - Perform filter and group by aggregation
+    
+
+    // 3 - Write results back to csv file
+
+    // 3.1 Similarly to reading arguments we also need to have "csv_write_args"
+    // Reference: 
 
     return 0;
 }
