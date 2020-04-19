6efbd8fd111794f29708d174c9f1a0d779585a01
Jeremy Dyer <jdye64@gmail.com>
2020-04-19 14:17:36 -0400

    Filter to keep on PRCP weather events

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 #include <cudf/scalar/scalar.hpp>
 #include <cudf/copying.hpp>
 
-#include <cudf/detail/copy_if.cuh>
+#include <cudf/stream_compaction.hpp>
 
 
 int main() {
@@ -40,45 +40,25 @@ int main() {
     // 1.3 Perform the actual read which create the cudf::table instance for you. Same thing as Python "DataFrame" instance
     io::table_with_metadata wdf = io::read_csv(in_args);
 
-    // // 1.4 This is not required its simply here to illustrate how info about the resulting table instance can be obtained
-    // setlocale(LC_NUMERIC, "");  // Simply for thousands separator
-    // printf("Num Columns: %'d Number of Rows: %'d\n", wdf.tbl->num_columns(), wdf.tbl->num_rows());
-    
-    // // Just an example of how to retrieve the table column names
-    // std::vector<std::string> column_names = wdf.metadata.column_names;
-
     // 2 - Weather data Logic section
     
     // 2.1 - Only keep 'PRCP' (Precipitation) events
-    using TypeOut = bool;
-    using TypeLhs = std::string;
-    using TypeRhs = std::string;
+    std::unique_ptr<cudf::column> prcp_cols = cudf::experimental::binary_operation(cudf::string_scalar("PRCP"), 
+                                                       wdf.tbl->view().column(2), // "observation_type" column
+                                                       cudf::experimental::binary_operator::EQUAL,
+                                                       cudf::data_type(cudf::experimental::type_to_id<bool>()));
 
-    // cudf::experimental::binary_operator eq_precip_binop = cudf::experimental::binary_operator::EQUAL;
-    //cudf::string_scalar prcp_scalar("PRCP");
+    // std::vector<std::unique_ptr<cudf::column>> cols;
+    // cols.push_back(std::move(result));
+    // cudf::experimental::table prcp_events = cudf::experimental::table(std::move(cols));
 
-    // auto result = cudf::experimental::binary_operation(prcp_scalar, 
-    //                                                    wdf.tbl->view().column(2), // "observation_type" column
-    //                                                    cudf::experimental::binary_operator::EQUAL,
-    //                                                    cudf::data_type(cudf::experimental::type_to_id<TypeOut>()));
-
-    //using EQUAL = cudf::library::operation::Equal<TypeOut, TypeLhs, TypeRhs>;
-
-    // Create a Filter/Functor to filter only for the desired "PRCP" weather events. Ensure __device__ is present to run in CUDA!
-    struct prcp_evt_functor {
-        bool operator()(cudf::size_type i) const { return true; }
-    };
-
-
-    std::unique_ptr<cudf::experimental::table> prcp_tbl = cudf::experimental::detail::copy_if(wdf.tbl->view(), prcp_evt_functor);
+    std::unique_ptr<cudf::experimental::table> prcp_tbl = cudf::experimental::apply_boolean_mask(wdf.tbl->view(), prcp_cols->view());
 
     // 3 - Write results back to parquet file
 
     // 3.1 Similarly to reading arguments we also need to have "parquet_write_args"
     // Reference:
     std::string output_parquet = "/home/jdyer/Development/libcudf-examples/data/weather/results.parquet";
-    //io::write_parquet_args out_args{io::sink_info{output_parquet}, wdf.tbl->view()};
-    //std::make_uni<cudf::experimental::table> out_table = 
     io::write_parquet_args out_args{io::sink_info{output_parquet}, prcp_tbl->view()};
 
     // Actually write the parquet data to file
